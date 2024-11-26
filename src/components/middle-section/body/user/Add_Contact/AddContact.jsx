008dcff9e1b54dd72597808e0d6203e6e2fd401c
Suhag <suhagsundarswain1@gmail.com>
2024-11-26 18:08:51 +0530

    v1.8.0-Add contact page added

--- src/components/middle-section/body/user/Add_Contact/AddContact.jsx
+++ src/components/middle-section/body/user/Add_Contact/AddContact.jsx
@@ -1,4 +1,31 @@
+import { useSelector } from "react-redux";
+import styles from "./AddContact.module.css";
+import AddContactForm from "./AddContactForm";
+import PersonIcon from "@mui/icons-material/Person";
+
 const AddContact = () => {
-  return <>Add Contact</>;
+  const theme = useSelector((store) => store.theme);
+  return (
+    <div className={styles.addContact}>
+      <div
+        className={`${styles.addContactForm} ${
+          theme.lightTheme
+            ? styles.addContactForm_light
+            : styles.addContactForm_dark
+        }`}
+      >
+        <div className={styles.form_title}>
+          <h2 style={{ marginBottom: "0px" }}>Add Contact</h2>
+          <PersonIcon style={{height:"auto",width:"40px"}}/>
+        </div>
+
+        <p style={{ color: "grey" }}>
+          The contact will save and you can access from anywhere and can send
+          email.
+        </p>
+        <AddContactForm />
+      </div>
+    </div>
+  );
 };
 export default AddContact;
---
+++ src/components/middle-section/body/user/Add_Contact/AddContactForm.jsx
@@ -0,0 +1,88 @@
+import styles from "./AddContactForm.module.css";
+import { InputAdornment, TextField } from "@mui/material";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import CallOutlinedIcon from "@mui/icons-material/CallOutlined";
+import { useSelector } from "react-redux";
+
+const AddContactForm = () => {
+  const theme = useSelector((store) => store.theme);
+  const form_fields = [
+    {
+      label: "Contact Name",
+      text_field_icon: <PersonOutlineOutlinedIcon />,
+      placeholder: "Enter contact name",
+    },
+    {
+      label: "Contact Email",
+      text_field_icon: <EmailOutlinedIcon />,
+      placeholder: "Enter contact email",
+    },
+    {
+      label: "Contact Phone",
+      text_field_icon: <CallOutlinedIcon />,
+      placeholder: "Enter contact phone/mobile number",
+    },
+    {
+      label: "Contact Address",
+      type: "textarea",
+      placeholder: "Enter contact address",
+    },
+    {
+      label: "Contact Description",
+      type: "textarea",
+      placeholder: "Enter contact details/description.",
+    },
+  ];
+  return (
+    <form>
+      {form_fields.map((field) => (
+        <div key={field.label}>
+          <label className={styles.field_label}>{field.label + ":"}</label>
+          {field.type == "textarea" ? (
+            <textarea
+              className={`${styles.textField} ${
+                theme.lightTheme
+                  ? styles.textField_light
+                  : styles.textField_dark
+              }`}
+              placeholder={field.placeholder}
+            ></textarea>
+          ) : (
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              size="small"
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      {field.text_field_icon}
+                    </InputAdornment>
+                  ),
+                },
+              }}
+              placeholder={field.placeholder}
+              className={`${styles.textField} ${
+                theme.lightTheme
+                  ? styles.textField_light
+                  : styles.textField_dark
+              }`}
+              style={{ marginBottom: "20px" }}
+            />
+          )}
+        </div>
+      ))}
+      <div className={styles.button_container}>
+        <button type="submit" className={`btn ${theme.lightTheme?"btn-dark":"btn-primary"}`}>
+          Add contact
+        </button>
+        <button type="reset" className="btn btn-danger">
+          Reset
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AddContactForm;
